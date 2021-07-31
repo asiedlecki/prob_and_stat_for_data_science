0fc40a76efea6ccfd341100a3fe74fa6893d2958
Artur Siedlecki <art.fanrock@gmail.com>
2021-07-31 15:37:43 +0200

    Used function sample() for simulating all iterations

--- monte_carlo_simulation/monte_carlo.R
+++ monte_carlo_simulation/monte_carlo.R
@@ -4,17 +4,12 @@ probtotk <- function(d, k, nreps) {
   count <- 0
   # do the experiment nreps times -- like doing nreps notebook lines
   for (rep in 1:nreps) {
-    sum <- 0
     # roll d dice and find their sum
-    for (j in 1:d) sum <- sum + roll()
-    if (sum == k) count <- count + 1
+    total <- sum(sample(1:6, d, replace = TRUE))
+    if (total == k) count <- count + 1
   }
   return(count/nreps)
 }
-
-# simulate roll of one die; the possible return
-# values are 1,2,3,4,5,6, all equally likely
-roll <- function() return(sample(1:6, 1))
 
 # example
 probtotk(3, 8, 10000)
